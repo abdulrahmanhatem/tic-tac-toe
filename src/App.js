17c4ba8ccc054573c285b6507c3981a4cf78f4c0
Hatem Hekaia <abdelrahmanhatem.dev@gmail.com>
2024-02-24 12:58:02 +0200

    highlight winner squares

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import {useState} from "react";
 
-function Square({value, onSquareClick}){
+function Square({value, onSquareClick, win}){
+  const classList = `square ${win ? "win" : ""}`;
 
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+  return <button className={classList} onClick={onSquareClick}>{value}</button>;
 }
 
 export function Board({xIsNext, squares, onPlay}) {
   const winner = calculateWinner(squares);
   let status;
-  
-  winner ? status = "Winner: " + winner : status = "Next turn: " + (xIsNext ? "X" : "O")
+  winner ? status = "Winner: " + squares[winner[0]] : status = "Next turn: " + (xIsNext ? "X" : "O")
 
   function handleClick (i) {
     const nextSquares = squares.slice();
@@ -29,7 +29,12 @@ export function Board({xIsNext, squares, onPlay}) {
     <div className="status">{status}</div>
     <div className="list">
       {squares.map((square, i) => {
-        return <Square value={squares[i]} onSquareClick={() => handleClick(i)}/>;
+        let win;
+        if (winner) {
+          win = winner.includes(i) ? true : false;
+        }
+        
+        return <Square value={squares[i]} onSquareClick={() => handleClick(i)} key={i} win={win}/>;
       })}
     </div>
       
@@ -97,7 +102,8 @@ function calculateWinner(squares){
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+
+      return lines[i];
     }
   }
   return null
