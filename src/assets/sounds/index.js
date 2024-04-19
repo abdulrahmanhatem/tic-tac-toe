2e6b6df11e36536a3a69b719b53940c36056f511
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-04-19 02:40:41 +0200

    change sounds source to be url to work in gh pages

--- src/assets/sounds/index.js
+++ src/assets/sounds/index.js
@@ -1,7 +1,6 @@
-export {default as background}  from "./background.mp3";
-export {default as win}  from "./win.mp3";
-export {default as x}  from "./x.mp3";
-export {default as o}  from "./o.mp3";
-export {default as draw}  from "./draw.mp3";
-export {default as buzz}  from "./buzz.mp3";
+export const win = "https://raw.githubusercontent.com/abdulrahmanhatem/tic-tac-toe/main/src/assets/sounds/win.mp3";
+export const x = "https://raw.githubusercontent.com/abdulrahmanhatem/tic-tac-toe/main/src/assets/sounds/x.mp3";
+export const o =  "https://raw.githubusercontent.com/abdulrahmanhatem/tic-tac-toe/main/src/assets/sounds/o.mp3";
+export const draw = "https://raw.githubusercontent.com/abdulrahmanhatem/tic-tac-toe/main/src/assets/sounds/draw.mp3";
+export const buzz = "https://raw.githubusercontent.com/abdulrahmanhatem/tic-tac-toe/main/src/assets/sounds/buzz.mp3";
 
